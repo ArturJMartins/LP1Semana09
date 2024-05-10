b26b0aa132add0d827c7959c268054d9fb7c7ede
Artur Martins <arturjcmartins.dev@gmail.com>
2024-05-10 13:59:02 +0100

    Verificação de duplicados, utilzando enum e HashSet

---
+++ EqualPlayer/Player.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EqualPlayer
+{
+    public class Player
+    {
+        public PlayerClass PClass { get;}
+        public string Name  {get;}
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass = pClass;
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return PClass.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public override bool Equals (object obj)
+        {
+            Player other = obj as Player;
+            if (other is null) return false;
+            return PClass == other.PClass && Name == other.Name;
+        }
+    }
+}
