964d91e906dc5ade3992494f06b8a7c1e1d40e97
Artur Martins <arturjcmartins.dev@gmail.com>
2024-05-10 14:30:25 +0100

    Pedir ao utilizador o nome do ficheiro e escrever em ciclo

---
+++ FilePower1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Queue<string> queue = new Queue<string>();
+            
+            string palavras;
+            do
+            {
+                Console.WriteLine("Digite palavras e carregue 'enter'):");
+                palavras = Console.ReadLine();
+                if(palavras != "")
+                {
+                    queue.Enqueue(palavras);
+                }
+            } while (palavras != "");
+
+            File.WriteAllLines(args[0],queue);
+    
+        }
+    }
+}
