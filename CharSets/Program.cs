24fa97aa9670da7949e9598f09fd28b73b882016
Artur Martins <arturjcmartins.dev@gmail.com>
2024-05-10 17:11:14 +0100

    Ciclo de leitura dos ficheiros

---
+++ CharSets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CharSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Não foi adicionado nenhum ficheiro.");
+            }
+            else
+            {
+                HashSet<string> conjunto = new HashSet<string>();
+                string s;
+
+                foreach (string filename in args)
+                {
+                    using StreamReader sr = new StreamReader(filename);
+
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        conjunto.Add(s);
+                    }
+                }
+                
+            }
+    
+        }
+    }
+}
