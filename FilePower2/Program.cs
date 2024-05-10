e5af22c888296917c1f10d1513a1b7d228b3f5f9
Artur Martins <arturjcmartins.dev@gmail.com>
2024-05-10 14:59:52 +0100

    StreamWriter sem utilizar uma queue

---
+++ FilePower2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string input = " ";
+            //escrever no texto.txt
+            using StreamWriter sw = new StreamWriter(args[0]);
+
+            do
+            {
+                Console.WriteLine("Digite palavras e carregue 'enter'):");
+                if(input != "")
+                {
+                    input = Console.ReadLine();
+                    sw.WriteLine(input);
+                }
+            } while (input != "");
+    
+        }
+    }
+}
